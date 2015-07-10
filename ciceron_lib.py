1da2dc7002ac689b566ca35c28a01d6a33deebfc
Bryan Rhee <psy2848048@gmail.com>
2015-07-10 20:52:05 +0900

    WTF

--- ciceron_lib.py
+++ ciceron_lib.py
@@ -250,6 +250,7 @@ def json_from_V_REQUESTS(conn, rs, purpose="newsfeed"):
 
     for row in rs:
         request_id = row[0]
+        print row[0]
         # For fetching translators in queue
         cursor2 = conn.execute("SELECT * FROM V_QUEUE_LISTS WHERE request_id = ? ORDER BY user_id",
                 [request_id])
