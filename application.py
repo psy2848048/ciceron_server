cb346908616be803451e8231368059bdefcc3c33
Bryan Rhee <psy2848048@gmail.com>
2015-10-31 00:05:30 +0900

    Revise link of mail alarm

--- application.py
+++ application.py
@@ -113,78 +113,78 @@ def parallel_send_email(user_name, user_email, noti_type, request_id, language_i
     if noti_type == 0:
         message = template.translator_new_ticket(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa/%d' % request_id}
+             "link": HOST + '/stoa/%d' % request_id}
 
     elif noti_type == 1:
         message = template.translator_check_expected_time(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/translating/%d' % request_id,
+             "link": HOST + '/translating/%d' % request_id,
              "expected": optional_info.get('expected')}
             # datetime.now() + timedelta(seconds=(due_time - start_translating_time)/3)
 
     elif noti_type == 2:
         message = template.translator_complete(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/activity/%d' % request_id}
+             "link": HOST + '/activity/%d' % request_id}
             
     elif noti_type == 3:
         message = template.translator_exceeded_due(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa/%d' % request_id}
+             "link": HOST + '/stoa/%d' % request_id}
 
     elif noti_type == 4:
         message = template.translator_extended_due(langauge_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/translating/%d' % request_id,
+             "link": HOST + '/translating/%d' % request_id,
              "new_due": optional_info.get('new_due')}
 
     elif noti_type == 5:
         message = template.translator_no_answer_expected_time(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa/%d' % request_id}
+             "link": HOST + '/stoa/%d' % request_id}
 
     elif noti_type == 6:
         message = template.client_take_ticket(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/processingrequests/%d' % request_id,
+             "link": HOST + '/processingrequests/%d' % request_id,
              'hero': optional_info.get('hero')}
 
     elif noti_type == 7:
         message = template.client_check_expected_time(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/processingrequests/%d' % request_id}
+             "link": HOST + '/processingrequests/%d' % request_id}
 
     elif noti_type == 8:
         message = template.client_giveup_ticket(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa/%d' % request_id,
+             "link": HOST + '/stoa/%d' % request_id,
              "hero": optional_info.get('hero')}
 
     elif noti_type == 9:
         message = template.client_no_answer_expected_time_go_to_stoa(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa/%d' % request_id}
+             "link": HOST + '/stoa/%d' % request_id}
 
     elif noti_type == 10:
         message = template.client_complete(language_id) %{"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/donerequests/%d' % request_id,
+             "link": HOST + '/donerequests/%d' % request_id,
              "hero": optional_info.get('hero')}
 
     elif noti_type == 11:
         message = template.client_incomplete(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/processingrequests/%d' % request_id}
+             "link": HOST + '/processingrequests/%d' % request_id}
 
     elif noti_type == 12:
         message = template.client_no_hero(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/processingrequests/%d' % request_id}
+             "link": HOST + '/processingrequests/%d' % request_id}
 
     elif noti_type == 14:
         message = template.client_no_hero(language_id) % {"host": os.environ.get('HOST', app.config['HOST']),
              "user": user_name,
-             "link": '/stoa'}
+             "link": HOST + '/stoa'}
 
     send_mail(user_email, "Here is your news, %s" % user_name, message)
 
