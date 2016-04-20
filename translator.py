a2373b743d94cd5caaeea4f8e7ea4f2bc257d017
Bryan Rhee <psy2848048@gmail.com>
2016-04-20 09:10:00 +0900

    Bing도 활성화

--- translator.py
+++ translator.py
@@ -8,9 +8,9 @@ from microsofttranslator import Translator as Bing_Translator
 class Translator:
     def __init__(self):
         self.googleAPI = build('translate', 'v2',
-                                developerKey='AIzaSyDIyeO9auTHO6qqciEqsmZLexZtQ9kpey0')
+                                developerKey='AIzaSyD-S4_2g1SRp4jucHpdLSBBq6xWhOsHcSI')
         #self.yandexAPI = ...
-        self.bingAPI = Bing_Translator('<Your Client ID>', '<Your Client Secret>')
+        self.bingAPI = Bing_Translator('welcome_ciceron', 'VL9isREJUILWMCLE2hr75xVaePRof6kuGkCM+r9oTb0=')
 
     def _googleTranslate(self, source_lang, target_lang, sentences):
         result_google = self.googleAPI.translations().list(
@@ -18,31 +18,27 @@ class Translator:
                                                 target=target_lang,
                                                      q=sentences
                 ).execute()
-        result_array = [ item['translatedText'] for item in result_google ]
-        return result_google
+        result_array = result_google['translations'][0]['translatedText']
+        return result_array
 
     def _bingTranslate(self, source_lang, target_lang, sentences):
         result_bing = self.bingAPI.translate(sentences, target_lang)
 
-        result_array = [ item['TranslatedText'] for item in result_bing ]
+        #result_array = [ item['TranslatedText'] for item in result_bing ]
+        result_array = result_bing
         return result_array
 
     def doWork(self, source_lang, target_lang, sentences):
+        # Length limit:
+        #     Google: 2050 byte
+
         result_google = self._googleTranslate(source_lang, target_lang, sentences)
         result_bing = self._bingTranslate(source_lang, target_lang, sentences)
 
-        result = []
-        for google, bing in zip(result_google, result_bing):
-            item = {}
-            item['google'] = google
-            item['bing'] = bing
-
-            result.append(item)
-
-        return result
+        return {'google': result_google, 'bing': result_bing}
 
 if __name__ == '__main__':
     translator = Translator()
-    requests = ["""It went like this: it said, "Hello James Veitch, I have an interesting business proposal I want to share with you, Solomon." Now, my hand was kind of hovering on the delete button, right? I was looking at my phone. I thought, I could just delete this. Or I could do what I think we've all always wanted to do."""]
-    result = translator.doWork('en', 'ko', requests)
-    print result
+    requests = u"""논문의 평균 분량은 분야마다 다 다르다. 수학 같은 경우는 정말 A4용지 반 장 분량(…)의 논문이라고 하더라도 그 내용이 어떠한가에 따라서 세계를 발칵 뒤집는 불후의 논문이 될 수도 있다.[2] 사회과학은 그보다는 좀 더 길어진다. 대개의 심리학 논문은 20~30장 선에서 어지간하면 글이 끝나고, 정치학은 비슷하거나 그보다는 좀 더 긴 편이다. 논문의 방대함으로는 (연구주제에 따라서는) 행정학이 유명한데, 이 분야는 나랏님 하시는 일을 다루는지라 일단 데이터 양부터가 장난이 아니다. 오죽하면 행정학자들끼리 "우리는 학회를 한번 갔다오면 왜 연구실에 전화번호부 두께의 학회지가 너댓 편씩 쌓이지?"(…) 같은 농담을 주고받을 정도이니...[3] 그 외에도 논문 분량이 당연히 백여 페이지를 한참 넘을 것으로 기대되는 분야들은 꽤 있다. 단, 학술지 논문에 비해 우리 위키러들이 정말로 궁금할 학위논문의 경우 분량이 그 5~10배 가량 육박하는 경우가 많으니 참고. 일부 박사논문은 납본되는 걸 보면 정말로 책 한 권이 나온다.(...)  좀 심하게 말하면, 어떤 학술적인 글을 쓰는데 분량을 신경쓰는 것은 레포트 쓰는 학부생들의 수준에서 바라보는 시각일 수 있다. (굳이 좋게 평하자면, 최소한의 논문다운 논문을 쓰기 위한 휴리스틱이다.) 학계에서 논문의 가치는 그 논문의 양이 얼마나 방대한지는 전혀 상관없다. 일부 사회과학 분야 논문들은 가설을 한번에 30개 이상씩(!) 검증하기도 하나, 그런 논문이 가설 하나 검증하는 논문, 아니 아무도 신경쓰지 않은 문제를 최초로 제기하느라 가설은 아예 검증하지도 못하고 제안하기만 한 논문보다 우월하다고 취급되지는 않는다. 가설을 많이 검증한다고 해도 그 검증과정이나 논리적 차원에서 결함이나 비약이 있다면 가차없이 탈탈 털릴 뿐이다. 원론적으로, 인문학이나 예술분야라고 해도 자신의 독창적 생각을 타인에게 설득력 있게 전달하는 과정이 중요하게 취급되는 것은 당연하다.  공연히 분량을 늘린답시고 논문에서 논거를 질질 끌거나 쓸데없는 데이터를 넣거나 하면 당연히 또 탈탈 털린다. 애초에 학계라는 곳은 타인의 언급을 인용하는 것조차도 논리적 전개에 불필요해 보인다 싶으면 가차없이 불벼락을 내리는 바닥이다.[4] 필요한 말을 안 써서 까이기도 하지만, 쓸데없는 말이 너무 많다고 까이기도 하니, 논문을 준비하는 연구자는 이래저래 피곤하다. 게다가 교수들도 긴 글 읽기는 싫어하는 경우가 많다.(…)[5] """
+    result = translator.doWork('ko', 'en', requests)
+    print (result)
