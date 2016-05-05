2501276d8ef6457a0f4e85a93d12c41d9ae264d9
Bryan Rhee <psy2848048@gmail.com>
2016-05-05 15:15:58 +0900

    마로찡 빨리와 ㅜㅜ

--- logTransfer.py
+++ logTransfer.py
@@ -7,7 +7,7 @@ class LogTransfer:
     def __init__(self, dbInfo):
         self.conn = psycopg2.connect(dbInfo)
 
-    def logTransfer(self, conn):
+    def run(self):
         cursor = self.conn.cursor()
         
         query_getmax = "SELECT MAX(id) FROM CICERON.TEMP_ACTIONS_LOG"
@@ -32,3 +32,11 @@ if __name__ == "__main__":
         DATABASE = "host=ciceronprod.cng6yzqtxqhh.ap-northeast-1.rds.amazonaws.com port=5432 dbname=ciceron user=ciceron_web password=%s"
     else:
         DATABASE = "host=cicerontest.cng6yzqtxqhh.ap-northeast-1.rds.amazonaws.com port=5432 dbname=ciceron user=ciceron_web password=%s"
+
+    parser = argparse.ArgumentParser(description='Translation agent')
+    parser.add_argument('--dbpass', dest='dbpass', help='DB password')
+    args = parser.parse_args()
+
+    dbInfo = DATABASE % args.dbpass
+    logTransfer = LogTransfer(dbInfo)
+    logTransfer.run()
