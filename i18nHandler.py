9221095f41032ac308d6bc7ba339c3c685a79094
Bryan Rhee <psy2848048@gmail.com>
2016-06-26 17:54:51 +0900

    DB setting

--- i18nHandler.py
+++ i18nHandler.py
@@ -7,20 +7,96 @@ class I18nHandler(object):
     def __init__(self, conn):
         self.conn = conn
 
+    def __getCountryNameById(self, lang_id):
+        query = "SELECT text FROM CICERON.D_LANGUAGES WHERE id = %s"
+        cursor = self.conn.cursor()
+
+        cursor.execute(query, (lang_id, ))
+        res = cursor.fetchone()
+        if res is None or len(res) == 0:
+            return "null"
+        else:
+            return res[0]
+
+    def __getCountryCodeById(self, lang_id):
+        query = "SELECT google_code FROM CICERON.D_LANGUAGES WHERE id = %s"
+        cursor = self.conn.cursor()
+
+        cursor.execute(query, (lang_id, ))
+        res = cursor.fetchone()
+        if res is None or len(res) == 0:
+            return "null"
+        else:
+            return res[0]
+
+    def __getIdByCountryName(self, country_name):
+        query = "SELECT id FROM CICERON.D_LANGUAGES WHERE text = %s"
+        cursor = self.conn.cursor()
+
+        cursor.execute(query, (country_name, ))
+        res = cursor.fetchone()
+        if res is None or len(res) == 0:
+            return -1
+        else:
+            return res[0]
+
+    def __getIdByCountryCode(self, country_code):
+        query = "SELECT id FROM CICERON.D_LANGUAGES WHERE google_code = %s"
+        cursor = self.conn.cursor()
+
+        cursor.execute(query, (country_code, ))
+        res = cursor.fetchone()
+        if res is None or len(res) == 0:
+            return -1
+        else:
+            return res[0]
+
+    def __getLangCodesByRequestId(self, request_id):
+        query = """
+            SELECT original_lang_id, target_lang_id
+              FROM CICERON.F_REQUESTS
+              WHERE id = %s"""
+        cursor = self.conn.cursor()
+        cursor.execute(query, (request_id, ))
+        res = cursor.fetchone()
+
+        if res is None or len(res) == 0:
+            return -1
+        else:
+            return (res[0], res[1])
+
     def __insertToDB(self, request_id, variable_id, source_lang_id, target_lang_id, paragraph_seq, sentence_seq, text):
-        pass
+        cursor = self.conn.cursor()
+        query = """
+            INSERT INTO CICERON.D_I18N_REQUESTS
+                (id, request_id, variable_id, %(source_id)s, %(target_lang_id)s, paragraph_seq, sentence_seq, text)"""
 
     def __updateText(self, request_id, variable_id, source_lang_id, target_lang_id, paragraph_seq, sentence_seq, text):
+        pass
+
+    def __updateVariable(self, request_id, variable_id, source_lang_id, target_lang_id, paragraph_seq, sentence_seq, new_variable):
         pass
 
     def __deleteLine(self, request_id, variable_id, source_lang_id, target_lang_id, paragraph_seq, sentence_seq):
         pass
 
     def __dictToDb(self, request_id, dictData):
-        pass
+        for key, text in iteritems(dictData):
+            self.__insertToDB(request_id, key)
 
     def __dbToDict(self, request_id):
-        pass
+        query_db = "SELECT ..."
+
+        self.conn.execute(query_db, (request_id, ))
+        res = self.conn.fetchall()
+
+        dictObj = []
+        for key, text in res:
+            row = {}
+            row[key] = text
+            dictObj.append(row)
+
+        return dictObj
 
     def _iosToDict(self, iosText):
         pass
