5f26734ddb4a03a6ffde7bb12e6521672179d7f7
Bryan Rhee <psy2848048@gmail.com>
2016-08-18 22:02:11 +0900

    으아아
    퍼블릭번역 재판매 모듈은 만들었는데...
    이것에 맞게 엊그제 했던 노가다를 다시 해야함......!!!!!!!!!!!!!!!!!!!!
    
    근데 모듈화 해놓으니깐 앞으로의 구현 자체는 그렇게 어렵지는 않을듯

--- src/requestResell.py
+++ src/requestResell.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+import ciceron_lib
 
 
 class RequestResell(object):
@@ -5,10 +7,48 @@ class RequestResell(object):
         self.conn = conn
 
     def getList(self):
-        pass
+        cursor = self.conn.cursor()
+        query = """
+                WITH public_requests AS
+                (
+                    SELECT * FROM CICERON.V_REQUESTS
+                    WHERE status_id = 2
+                      AND is_public = true
+                    ORDER BY request_id DESC
+                )
 
-    def getReadPermission(self, request_id, user_id):
-        pass
+               SELECT * FROM public_requests
+               OFFSET FLOOR(RANDOM() * (SELECT COUNT(*) FROM public_requests))
+               LIMIT 20
+            """
+        cursor.execute(query)
+        res = cursor.fetchall()
+        return res
 
-    def readOneRequest(self, request_id):
-        pass
+    def setReadPermission(self, request_id, user_id):
+        cursor = self.conn.cursor()
+
+        new_id = ciceron_lib.get_new_id(self.conn, "F_READ_PUBLIC_REQUESTS_USERS")
+        query = """
+            INSERT INTO CICERON.F_READ_PUBLIC_REQUESTS_USERS
+            (id, request_id, user_id, is_paid)
+            VALUES
+            (%s, %s, %s, false)
+            """
+
+        cursor.execute(query, (new_id, request_id, user_id, ))
+
+    def setToPaid(self, request_id, user_id, payment_platform, transaction_id):
+        cursor = self.conn.cursor()
+
+        query = """
+            UPDATE CICERON.F_READ_PUBLIC_REQUESTS_USERS
+            SET   is_paid = true
+                , payment_platform = %s
+                , transaction_id = %s
+            WHERE request_id = %s
+              AND user_id = %s
+              AND is_paid = false
+            """
+
+        cursor.execute(query, (payment_platform, transaction_id, request_id, user_id, ))
