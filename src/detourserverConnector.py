83397b6a16c3e46b6a01c456355c914aa7cb2160
Bryan RHEE <psy2848048@gmail.com>
2016-08-30 00:08:39 +0000

    병렬처리는 쉬운데 병렬처리 결과를 하나로 모으는게 뷁이네

--- src/detourserverConnector.py
+++ src/detourserverConnector.py
@@ -3,13 +3,10 @@
 import requests
 import json
 import ciceron_lib
-from multiprocessing import Process
+from multiprocessing import Process, Array
 
 
 class Connector:
-    def __init__(self):
-        self.tempTranslatedData = {}
-
     def getTranslatedData(self, sentence, source_lang_id, target_lang_id):
         payload = {
                  'user_email':'admin@sexycookie.com'
@@ -41,8 +38,9 @@ class Connector:
 
     def getTranslatedDataParallel(self, conn, user_id, request_id, sentences, source_lang_id, target_lang_id):
         is_real_translator = ciceron_lib.strict_translator_checker(conn, user_id, request_id)
+        sharedArray = Array(dict, len(sentences))
 
-        def parallelJob(main_storage, idx, unit_sentence, lang_id1, lang_id2):
+        def parallelJob(arr, idx, unit_sentence, lang_id1, lang_id2):
             payload = {
                      'user_email':'admin@sexycookie.com'
                    , 'sentence': unit_sentence
@@ -51,11 +49,11 @@ class Connector:
                    , 'where': 'somewhere'
                    }
             response = requests.post('http://52.196.164.64/translate', data=payload)
-            main_storage[ ("%s" % idx) ] = json.loads(response.text)
+            arr[idx] = json.loads(response.text)
 
         job_storage = []
-        for idx, sentence in sentences:
-            job_storage.append( Process(target=parallelJob, args=(self.tempTranslatedData, idx, sentence, source_lang_id, target_lang_id, )) )
+        for idx, sentence in enumerate(sentences):
+            job_storage.append( Process(target=parallelJob, args=(sharedArray, idx, sentence, source_lang_id, target_lang_id, )) )
 
         for proc in job_storage:
             proc.start()
@@ -63,14 +61,16 @@ class Connector:
         for proc in job_storage:
             proc.join()
 
+        print self.tempTranslatedData
+
         result = {
                 'google': []
               , 'bing': []
               , 'yandex': []
                 }
-        for idx in xrange(self.tempTranslatedData):
-            result['google'].append(self.tempTranslatedData[ ("%s" % idx) ]['google'])
-            result['bing'].append(self.tempTranslatedData[ ("%s" % idx) ]['bing'])
-            result['yandex'].append(self.tempTranslatedData[ ("%s" % idx) ]['yandex'])
+        for idx in xrange(len(sharedArray)):
+            result['google'].append(sharedArray[idx]['google'])
+            result['bing'].append(sharedArray[idx]['bing'])
+            result['yandex'].append(sharedArray[idx]['yandex'])
 
         return result
