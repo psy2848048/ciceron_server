f1e16a7c3a99ae4631a4c5547abbdecedc629126
Bryan Rhee <psy2848048@gmail.com>
2016-09-12 02:19:55 -0700

    Indent 조절

--- src/payment.py
+++ src/payment.py
@@ -17,12 +17,12 @@ from requestResell import RequestResell
 ZERO = timedelta(0)
 
 class UTC(tzinfo):
-  def utcoffset(self, dt):
-    return ZERO
-  def tzname(self, dt):
-    return "UTC"
-  def dst(self, dt):
-    return ZERO
+    def utcoffset(self, dt):
+        return ZERO
+    def tzname(self, dt):
+        return "UTC"
+    def dst(self, dt):
+        return ZERO
 
 utc = UTC()
 
